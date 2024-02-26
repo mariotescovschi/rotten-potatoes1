09202cc9352452b4a5d51806dd407970462d5e65
Alin <scoban_alyn@yahoo.com>
2024-02-26 20:55:57 +0200

    am dat tab

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import {NavigationContainer} from "@react-navigation/native";
 export default function App() {
   return (
       <NavigationContainer>
-    <AppNavigator/>
+        <AppNavigator/>
       </NavigationContainer>
   );
 }
